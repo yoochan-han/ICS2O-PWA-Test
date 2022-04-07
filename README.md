30a2a323cd69fc7cae02ac98dd69d01879ab8fe9
Patrick Coxall <mr.coxall@mths.ca>
2022-04-07 11:38:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ICS2O-PWA-Template-HTML
+
+[![Mr Coxall's Super Linter](https://github.com/mr-coxall/github-page-pwa/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/mr-coxall/github-page-pwa/actions)
+
+[![Run on Repl.it](https://repl.it/badge/github/mr-coxall/github-page-pwa)](https://repl.it/github/mr-coxall/github-page-pwa)
+
+This site can be found at: [https://mr-coxall.github.io/github-page-pwa/](https://mr-coxall.github.io/github-page-pwa/)
---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/github-page-pwa/sw.js", {
+    scope: "/github-page-pwa/",
+  })
+}
+
+/**
+ * This function displays an alert.
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+}
